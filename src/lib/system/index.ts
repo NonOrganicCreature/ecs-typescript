3851ef3e938c07d1194daef7d62f9f3057c864f1
Daniil Poliakov <nonorganiccreature@gmail.com>
2022-08-18 08:47:32 +0400

    return type of system types

--- src/lib/system/index.ts
+++ src/lib/system/index.ts
@@ -1,13 +1,14 @@
+import { TComponent } from "../component";
 import { TEntity } from "../entity";
 import { ECSWorld } from "../world";
 
 export declare type TSystemCreationParams = {
-  requiredComponents: Array<new (...args: any[]) => any>;
+  requiredComponents: Array<new (...args: any[]) => TComponent>;
   run: (e: TEntity, ecsWorldRef: ECSWorld) => void;
 };
 
 export declare type TSystem = {
-  requiredComponents: Array<new (...args: any[]) => any>;
+  requiredComponents: Array<new (...args: any[]) => TComponent>;
   run: (e: TEntity) => void;
 };
 
